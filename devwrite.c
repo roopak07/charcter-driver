ac9d91dc1d66d45cc02ff5e1bd47d77dc65a3259
Roopak <h.rupak61@gmail.com>
2019-10-03 23:29:21 +0530

    Add files via upload

---
+++ devwrite.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+void main()
+{
+char buf[100];
+int fd = open("/dev/mydev",O_WRONLY);
+printf("open fd = %d \n",fd);
+ if(fd<0)
+ {
+  printf("open ERROR... EXIT\n");
+  exit(1);
+ }
+printf("Enter data to write: /n");
+scanf("%s",buf);
+write(fd,buf,strlen(buf)+1);
+}
