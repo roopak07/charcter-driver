0c7aad040064b1191b4e64352bf43b7e8a671162
Roopak <h.rupak61@gmail.com>
2020-05-08 17:32:26 +0530

    Create driver_file.c

---
+++ Basic loadable kernels/Licence_&_auth/driver_file.c
@@ -0,0 +1,21 @@
+#include<linux/kernel.h>
+#include<linux/module.h>
+#include<linux/init.h>
+
+int myinit(void)
+{
+printk(KERN_ALERT"Init function");
+return 0;
+}
+
+void myexit(void)
+{
+printk(KERN_ALERT"Exit function");
+}
+
+module_init(myinit);
+module_exit(myexit);
+MODULE_AUTHOR("Roopak");
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("Sample Driver");
+MODULE_SUPPORTED_DEVICE("Linux");
